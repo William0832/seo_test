7d71a1caec68fbace10a7e72d6df102e5cc8343c
William0832 <squall832@gmail.com>
2022-11-12 03:21:55 +0800

    🐞 fix: prot

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 const app = express()
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // middleware 
 const checkUserAgent = (req, res, next) => {
